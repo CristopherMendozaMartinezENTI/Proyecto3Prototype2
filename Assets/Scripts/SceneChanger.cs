efe5fead49ddb9e81c2bb489b96f88a33a0c11c1
TheKrizXP1 <thekrisxp1@gmail.com>
2021-10-19 01:44:04 +0200

    2 Demos on Build

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}

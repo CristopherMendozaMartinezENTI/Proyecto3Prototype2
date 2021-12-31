8541d0a8b1f22f7e3c4f1d8dec10aeb1b0db5523
TheKrizXP1 <thekrisxp1@gmail.com>
2021-12-31 02:21:17 +0100

    Antimatter Force Field VFX Added

--- Assets/Scripts/Environment/ObjectPositionResetter.cs
+++ Assets/Scripts/Environment/ObjectPositionResetter.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class ObjectPositionResetter : MonoBehaviour
 {
     [SerializeField] private GameObject playerTelekinesis;
+    [SerializeField] private GameObject collisionTrailsPrefab;
+    private GameObject _collisionTrailsTmp;
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<PropPhysicsController>() != null)
         {
+            _collisionTrailsTmp = Instantiate(collisionTrailsPrefab, other.transform.position, other.transform.rotation);
             other.GetComponent<PropPhysicsController>().ResetPos();
             if (playerTelekinesis.GetComponent<TelekinesisController>().IsObjectGrabbed())
                 playerTelekinesis.GetComponent<TelekinesisController>().ReleaseObject();
         }
+    }
+
+    private void Update()
+    {
+        Destroy(_collisionTrailsTmp, 5.0f);
     }
 }

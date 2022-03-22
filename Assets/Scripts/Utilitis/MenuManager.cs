29e075df2b1a8da017f3f7c0fd34f918dd69b40b
Cristopher Mendoza <thekrisxp1@gmail.com>
2022-03-22 04:07:28 +0100

    Main Menu Added + Chapter 1 Minor Fixes on Geometry

--- Assets/Scripts/Utilitis/MenuManager.cs
+++ Assets/Scripts/Utilitis/MenuManager.cs
@@ -7,11 +7,66 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    private void Update()
+    [SerializeField] private GameObject playCamera;
+    [SerializeField] private GameObject optionsCamera;
+    [SerializeField] private GameObject creditsCamera;
+    [SerializeField] private GameObject returnButton;
+    [SerializeField] List<GameObject> uiElements;
+
+    private void LoadScene()
     {
-        if(Input.GetKey(KeyCode.Space))
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, sceneName));
+    }
+
+    public void startGame()
+    {
+        StartCoroutine(enablePlayCamera());
+    }
+
+    IEnumerator enablePlayCamera()
+    {
+        foreach(GameObject uiElement in uiElements)
         {
-            StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, sceneName));
+            uiElement.SetActive(false);
         }
+        playCamera.SetActive(true);
+        yield return new WaitForSeconds(1);
+        LoadScene();
+    }
+
+    public void enableOptionsCamera()
+    {
+        foreach (GameObject uiElement in uiElements)
+        {
+            uiElement.SetActive(false);
+        }
+        optionsCamera.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    public void enableCreditsCamera()
+    {
+        foreach (GameObject uiElement in uiElements)
+        {
+            uiElement.SetActive(false);
+        }
+        creditsCamera.SetActive(true);
+        returnButton.SetActive(true);
+    }
+
+    public void enableMainCamera()
+    {
+        foreach (GameObject uiElement in uiElements)
+        {
+            uiElement.SetActive(true);
+        }
+        optionsCamera.SetActive(false);
+        creditsCamera.SetActive(false);
+        returnButton.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }

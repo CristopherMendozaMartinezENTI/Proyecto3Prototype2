7609d29f4c56d2f13c85a87650275a0da05dedfb
RSANFA <rsanfeliu@enti.cat>
2022-05-02 10:06:08 +0200

    Auto stash before merge of "AA3_DemoMusic" and "origin/AA3_DemoMusic"

--- Assets/Scenes/AA3_MusicDemo/AA3_sounds/AudioButtonUI.cs
+++ Assets/Scenes/AA3_MusicDemo/AA3_sounds/AudioButtonUI.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class AudioButtonUI : MonoBehaviour
 {
+
+    public AudioClip clipSound;
+    private AudioSource aSource;
+    public Button btn;
     public void Start()
     {
-        Button b = GetComponent<Button>();
-        AudioSource audio = GetComponent<AudioSource>();
-        b.onClick.AddListener(delegate () { audio.Play(); });
+        aSource = GetComponent<AudioSource>();
+    }
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(EventSystem.current.currentSelectedGameObject.GetComponent<Button>() == btn)
+            {
+                PlaySound(clipSound);
+            }
+        }
+    }
+    
+
+    void PlaySound(AudioClip aClip)
+    {
+       aSource.PlayOneShot(aClip);
     }
 }
--- Assets/Scripts/Utilitis/PauseMenuManager.cs
+++ Assets/Scripts/Utilitis/PauseMenuManager.cs
@@ -34,23 +34,34 @@ public class PauseMenuManager : MonoBehaviour
             PauseMenuCanvas.SetActive(false);
         }
     }
-
-    public void ResetScene()
+    public void newResetScene()
     {
+        StartCoroutine(ResetScene());
+    }
+    public IEnumerator ResetScene()
+    {
+;
         gameIsPaused = false;
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        yield return new WaitForSeconds(1);
         PauseMenuCanvas.SetActive(false);
         StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, SceneManager.GetActiveScene().name));
     }
-    public void ExitScene()
+    public void newExitScene()
+    {
+        StartCoroutine(ExitScene());
+    }
+    public IEnumerator ExitScene()
     {
         gameIsPaused = false;
         Time.timeScale = 1;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        yield return new WaitForSeconds(1);
         PauseMenuCanvas.SetActive(false);
         StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, "MainMenu"));
     }
+
 }

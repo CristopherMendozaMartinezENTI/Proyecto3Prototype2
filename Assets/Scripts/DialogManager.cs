f6a834e39db74988114cb8a154db9ad45e2f2d9e
Cristopher <thekrisxp1@gmail.com>
2022-06-07 13:05:31 +0200

    Dialogue System Rework

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using System.Linq;
 public class DialogManager : MonoBehaviour
 {
     [System.Serializable]
@@ -18,28 +19,63 @@ public class DialogManager : MonoBehaviour
         public AudioDialog[] Conversation;
     }
 
-    [SerializeField]
-    private List<AudioTrigger> triggersList = new List<AudioTrigger>();
-    public static List<AudioTrigger> staticTriggersList = new List<AudioTrigger>();
-    public static List<DialogTrigger> sortedList = new List<DialogTrigger>(); 
+    [SerializeField] private List<AudioTrigger> triggersList = new List<AudioTrigger>();
+    private List<DialogTrigger> sortedList = new List<DialogTrigger>();
 
+    private AudioSource source;
+    private GameObject UI_Subtitles;
     private void Start()
     {
-        staticTriggersList = triggersList;
-
-        foreach (DialogTrigger gameObj in GameObject.FindObjectsOfType<DialogTrigger>())
+        foreach (DialogTrigger gameObj in FindObjectsOfType<DialogTrigger>())
             if (gameObj.name.Contains("Dialog")) sortedList.Add(gameObj);
 
         sortedList.Sort( delegate (DialogTrigger dialogTriggerFirst, DialogTrigger dialogTriggerSecond)
         {
             return dialogTriggerFirst.name.CompareTo(dialogTriggerSecond.name);
         });
-    }
 
+        source = gameObject.GetComponent<AudioSource>();
+        UI_Subtitles = GameObject.Find("HUD").gameObject.transform.Find("Subtitles").gameObject;
+    }
+    private void Update()
+    {
+        if (Time.timeScale == 0)
+            source.Pause();
+        else if (Time.timeScale != 0 && !source.isPlaying)
+            source.Play();
+    }
+    public void PlayDialogue(string triggerName)
+    {
+        StartCoroutine(setSubtitles(triggerName));
+    }
+    public IEnumerator setSubtitles(string _triggerName)
+    {
+        float clipLeght = 0;
+        AudioTrigger temp = triggersList.Where(obj => obj.Trigger.name == _triggerName).SingleOrDefault();
+        DialogTrigger tempDialogTrigger = sortedList.Where(obj => obj.name == _triggerName).SingleOrDefault();
+
+        for (int i = 0; i < temp.Conversation.Length; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            UI_Subtitles.SetActive(true);
+            for (int j = 0; j < sortedList.Count; j++)
+            {
+                if (sortedList[j].name == _triggerName)
+                {
+                    UI_Subtitles.GetComponent<Text>().text = triggersList[j].Conversation[i].Dialog;
+                    source.clip = triggersList[j].Conversation[i].Audio;
+                    clipLeght = source.clip.length;
+                    yield return new WaitForSeconds(clipLeght - 0.4f);
+                    break;
+                }
+            }
+        }
+        UI_Subtitles.SetActive(false);
+        source.clip = null;
+    }
     private void OnDestroy()
     {
         triggersList.Clear();
-        staticTriggersList.Clear();
         sortedList.Clear();
     }
 }
--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -1,57 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 
 public class DialogTrigger : MonoBehaviour
 {
-    [SerializeField] private int dialogLenght;
-    private GameObject UI_Subtitles;
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
-        {
-            gameObject.GetComponent<AudioSource>().enabled = true;
-            UI_Subtitles = GameObject.Find("HUD").gameObject.transform.Find("Subtitles").gameObject;
-            StartCoroutine(setSubtitles());
-        }
+            FindObjectOfType<DialogManager>().PlayDialogue(gameObject.name);
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
-            gameObject.GetComponent<BoxCollider>().enabled = false;
-    }
-
-    private void Update()
-    {
-        if (Time.timeScale == 0)
-            gameObject.GetComponent<AudioSource>().Pause();
-        else if (Time.timeScale != 0 && !gameObject.GetComponent<AudioSource>().isPlaying)
-            gameObject.GetComponent<AudioSource>().Play();
-    }
-
-    private IEnumerator setSubtitles()
-    {
-        float clipLeght = 0;
-        for (int i = 0; i < dialogLenght; i++)
         {
-                yield return new WaitForSeconds(0.5f);
-                UI_Subtitles.SetActive(true);
-                for (int j = 0; j < DialogManager.sortedList.Count; j++)
-                {
-                    if (DialogManager.sortedList[j].name == gameObject.name)
-                    {
-                        UI_Subtitles.GetComponent<Text>().text = DialogManager.staticTriggersList[j].Conversation[i].Dialog;
-                        gameObject.GetComponent<AudioSource>().clip = DialogManager.staticTriggersList[j].Conversation[i].Audio;
-                        clipLeght = gameObject.GetComponent<AudioSource>().clip.length;
-                        yield return new WaitForSeconds(clipLeght - 0.4f);
-                        break;
-                    }
-                }                
+            gameObject.GetComponent<BoxCollider>().enabled = false;
         }
-        UI_Subtitles.SetActive(false);
-        gameObject.SetActive(false);
     }
 }
